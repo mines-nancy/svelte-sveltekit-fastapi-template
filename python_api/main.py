0a305eb19d3ed4bd5860cb07b7583994e01daf71
hugom <hugo.mazzitelli@viacesi;fr>
2023-01-20 16:46:24 +0100

    GPT Speech v1.0

--- python_api/main.py
+++ python_api/main.py
@@ -1,10 +1,13 @@
 from typing import Union
-from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import uvicorn
 from dotenv import load_dotenv
 import os
+
+from fastapi import FastAPI, Request
+import openai
+import json
 
 
 if os.getenv('ENV_MODE') == 'development':
@@ -24,31 +27,32 @@ print(allow_origins)
 ## CORS SECURITY
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=allow_origins,
+
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
 
 
-@app.get("/helloworld")
-async def helloword0(name: Union[str, None] = None, name2: Union[str, None] = None):
-    return {"HelloWorld": name2+name}
+openai.api_key = os.getenv('OPEN_AI_TOKEN_API')
 
-@app.get("/helloworld/{name}")
-async def helloword1(name:str):
-    return {"HelloWorld": name}
-
-
-class Person(BaseModel):
-    name: str
-    age: int
-    address: Union[str, None] = None
-
-@app.post("/helloworld")
-async def helloword2(person: Person):
-    return {"HelloWorld": person.name + " " + str(person.age)}
-
+@app.post("/predict")
+async def predict(request: Request):
+    data = await request.json()
+    text = data["text"]
+    print(text)
+    print("here")
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=(f"{text}"),
+        temperature=0.5,
+        max_tokens=4000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    return json.dumps(response["choices"][0]["text"])
 
 if __name__ == "__main__":
     uvicorn.run(
